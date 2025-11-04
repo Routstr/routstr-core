c49da9bf84c8f7377514beaeeeca8168cee611a8
9qeklajc <9qeklajc>
2025-11-04 23:50:55 +0100

    ignore error

--- routstr/proxy.py
+++ routstr/proxy.py
@@ -101,7 +101,7 @@ async def refresh_model_maps() -> None:
         }
 
         disabled_result = await session.exec(
-            select(ModelRow.id).where(ModelRow.enabled == False)
+            select(ModelRow.id).where(ModelRow.enabled == False)  # noqa: E712
         )
         disabled_model_ids = {row for row in disabled_result.all()}
 
