f4a9964a032f2935e46fd548a960d8e5c202749d
Evan Yang <evan@evanfy.com>
2026-03-03 13:03:27 +0800

    chore(release): bump version to 0.4.0

--- routstr/core/main.py
+++ routstr/core/main.py
@@ -35,9 +35,9 @@ setup_logging()
 logger = get_logger(__name__)
 
 if os.getenv("VERSION_SUFFIX") is not None:
-    __version__ = f"0.3.0-{os.getenv('VERSION_SUFFIX')}"
+    __version__ = f"0.4.0-{os.getenv('VERSION_SUFFIX')}"
 else:
-    __version__ = "0.3.0"
+    __version__ = "0.4.0"
 
 
 @asynccontextmanager
