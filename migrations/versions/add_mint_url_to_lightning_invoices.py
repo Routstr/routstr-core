d2641da38fb6ceced0fd5db604ae5f41f2ab5a20
9qeklajc <tthefux@gmail.com>
2026-07-10 23:07:34 +0200

    fix: linearize mint URL migration

--- migrations/versions/add_mint_url_to_lightning_invoices.py
+++ migrations/versions/add_mint_url_to_lightning_invoices.py
@@ -1,14 +1,14 @@
 """add mint_url to lightning_invoices
 
 Revision ID: add_mint_url_li
-Revises: c6d7e8f9a0b1
-Create Date: 2026-07-10 02:00:00.000000
+Revises: d7e8f9a0b1c2
+Create Date: 2026-07-10 23:07:19.000000
 """
 import sqlalchemy as sa
 from alembic import op
 
 revision = "add_mint_url_li"
-down_revision = "c6d7e8f9a0b1"
+down_revision = "d7e8f9a0b1c2"
 branch_labels = None
 depends_on = None
 
